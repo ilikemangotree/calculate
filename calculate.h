8cf999e6312c85c4960d4e673025cb9a8f2e41bb
leolkx123 <492644858@qq.com>
2017-02-07 12:09:34 +0800

    first commit

---
+++ calculate.h
@@ -0,0 +1,21 @@
+#ifndef CALCULATE_H
+#define CALCULATE_H
+class calculate
+{
+	public:
+		void A1();
+	    void B1();
+	    void B3();
+	    void C1();
+	private:
+		int d1;
+		int d2;
+		int d3;
+		int d4;
+		int c1;
+		int c2;
+		int c3;
+};
+
+#endif 
+
